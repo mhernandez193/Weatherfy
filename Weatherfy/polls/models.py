8d9b156634333333cf18ad5d5e84dba6f517b1f5
mhernandez193 <93899359+mhernandez193@users.noreply.github.com>
2022-09-26 14:13:04 -0500

    Add files via upload

--- Weatherfy/polls/models.py
+++ Weatherfy/polls/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import admin
 import datetime
 from django.utils import timezone
 
@@ -9,7 +10,16 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    	now = timezone.now()
+    	return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
@@ -18,3 +28,5 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+        
+
