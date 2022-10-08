1fde9fe00664de56f70d12dad3025c59d40a50b3
Joshua Blew <jblewdv@gmail.com>
2022-10-08 16:19:32 -0500

    feat: Spotify auth and environment variables

--- Weatherfy/base/views.py
+++ Weatherfy/base/views.py
@@ -1,21 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
 
+
 def home(request):
-	return render(request, 'base/home.html')
-	
+    return render(request, "base/home.html")
+
+
 def profile(request):
-	return render(request, 'base/profile.html')
-	
+    return render(request, "base/profile.html")
+
+
 def playlists(request):
-	return render(request, 'base/playlists.html')
-	
+    return render(request, "base/playlists.html")
+
+
 def location(request):
-	return render(request, 'base/location.html')
+    return render(request, "base/location.html")
+
 
 def friends(request):
-	return render(request, 'base/friends.html')
+    return render(request, "base/friends.html")
+
 
 def about(request):
-	return render(request, 'base/about.html')
+    return render(request, "base/about.html")
