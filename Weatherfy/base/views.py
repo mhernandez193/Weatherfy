d6fc59111e16d63d992f54b133f82751d573fbdc
Joshua Blew <jblewdv@gmail.com>
2022-10-11 12:36:31 -0500

    feat: connect all main views

--- Weatherfy/base/views.py
+++ Weatherfy/base/views.py
@@ -1,17 +1,68 @@
 from django.shortcuts import render
+
+import ipinfo
+import environ
+from datetime import datetime
+from random import randint
 from django.http import HttpResponse
 import folium
 import geocoder
 from .apis.Spotify import Spotify
+from .apis.Weather import fetchWeather
 
-
+env = environ.Env()
+environ.Env.read_env()
 
 
 def home(request):
+    city = None
+
+    # User searched weather for a different city
+    if request.method == 'POST':
+        city = request.POST['city']
+
+    # Current city
+    else:
+        # Grab IP address from request
+        if env("STAGE") == 'DEV':
+            ipAddress = '216.239.36.21'  # Mountain View, CA
+        else:
+            x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+            if x_forwarded_for:
+                ipAddress = x_forwarded_for.split(',')[0]
+            else:
+                ipAddress = request.META.get('REMOTE_ADDR')
+
+        # Fetch geolocation using IP
+        handler = ipinfo.getHandler(env("IP_INFO_ACCESS_TOKEN"))
+        details = handler.getDetails(ipAddress)
+        city = details.city
+
+    # Fetch weather for city
+    weather = fetchWeather(city)
+
+    # Fetch Spotify playlist using weather
     spotifyClient = Spotify()
-    playlist = spotifyClient.fetchPlaylistByKeyword('rainy')
+    playlist = spotifyClient.fetchPlaylistByKeyword(
+        weather['weather'][0]['description'])
+
+    # Create data payload
+    current_time = datetime.now()
+    formatted_time = current_time.strftime("%A, %B %d %Y, %H:%M:%S %p")
+
     data = {
-        "playlist": playlist[0].items[0].uri
+        "city": str(city),
+        "weather": {
+            'city': city,
+            'description': weather['weather'][0]['description'],
+            'icon': weather['weather'][0]['icon'],
+            'temperature': str(weather['main']['temp']) + ' °F',
+            'country_code': weather['sys']['country'],
+            'wind': 'Wind: ' + str(weather['wind']['speed']) + ' m/h',
+            'humidity': 'Humidity: ' + str(weather['main']['humidity']) + '%',
+            'time': formatted_time
+        },
+        "playlist": playlist[0].items[randint(0, playlist[0].total-1)]
     }
 
     return render(request, "base/home.html", data)
@@ -42,9 +93,12 @@ def location(request):
 	context={'m' : m,}
 	return render(request, 'base/location.html', context)
 
-
 def friends(request):
-    return render(request, "base/friends.html")
+    data = {
+        'friends': [],
+        'users': [],
+    }
+    return render(request, "base/friends.html", data)
 
 
 def about(request):
