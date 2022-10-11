b3469375fdcaea8e963539fab56f9c14ead79505
mhernandez193 <93899359+mhernandez193@users.noreply.github.com>
2022-10-11 13:12:04 -0500

    Update settings.py

--- Weatherfy/Weatherfy/settings.py
+++ Weatherfy/Weatherfy/settings.py
@@ -124,15 +124,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = "static/"
-MEDIA_URL = "media/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-
-MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
