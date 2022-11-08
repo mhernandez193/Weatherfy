eb64ded912c869078027ab350810379372c25769
mhernandez193 <93899359+mhernandez193@users.noreply.github.com>
2022-11-08 14:41:59 -0600

    Update views.py

--- Weatherfy/base/views.py
+++ Weatherfy/base/views.py
@@ -195,3 +195,46 @@ def friends(request):
 
 def about(request):
     return render(request, "base/about.html")
+
+def register(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+		form = CreateUserForm()
+		if request.method == 'POST':
+			form = CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				user = form.cleaned_data.get('username')
+				messages.success(request, 'Account Created: ' + user)
+
+				return redirect('login')
+			
+
+		context = {'form':form}
+		return render(request, 'base/register.html', context)
+    
+    
+def userLogin(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('home')
+			else:
+				messages.info(request, 'Username OR password is incorrect')
+
+		context = {}
+		return render(request, 'base/login.html', context)
+		
+		
+def userLogout(request):
+	logout(request)
+	return redirect('login')
