f89f39cb7662b1657571ef9a8cb17a936c357c62
mhernandez193 <93899359+mhernandez193@users.noreply.github.com>
2022-11-08 16:55:48 -0600

    Update views.py

--- Weatherfy/base/views.py
+++ Weatherfy/base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import messages
 import tekore as tk
 import ipinfo
 import environ
